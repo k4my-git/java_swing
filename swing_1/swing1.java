9f7bf97f05a6ac5dc24763d7abbc1531d12919b4
k4my-git <kambara0229@icloud.com>
2021-02-27 22:31:25 +0900

    first commit

---
+++ swing_1/swing1.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class swing1{
+    public static void main(String[] args){
+        JFrame frame = new JFrame("test");
+        frame.setSize(300,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
